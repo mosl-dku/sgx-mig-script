2d8da0d57f703d4fdc6fe9dd3e8eb09fc6769833
HoCheol Nam <43805697+namhc616@users.noreply.github.com>
2021-01-19 14:28:03 +0900

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 source ~mobileos/.bashrc
 source /opt/intel/sgxsdk/environment
-cd
 # need to be customized...
-~/SGX/SGXDataCenterAttestationPrimitives/SampleCode/QuoteGenerationSample
+cd ~/SGX/SGXDataCenterAttestationPrimitives/SampleCode/QuoteGenerationSample
 ./app
